66bdad9e9ba8a6166d5663393894d4a900b98884
ada1ra <adada0901@gmail.com>
2025-10-23 22:29:21 +0300

    Updated hw_5-1_proBrackets.c with names changes according to WebKit

--- src/hw_5-1_proBrackets/hw_5-1_proBrackets.c
+++ src/hw_5-1_proBrackets/hw_5-1_proBrackets.c
@@ -1,38 +1,35 @@
+#include "hw_5-1_proBrackets/hw_5-1_proBrackets.h"
+#include "stack/stack.h"
 #include <stdio.h>
 #include <string.h>
-#include "stack\stack.h"
-#include "hw_5-2_sortStation\hw_5-2_sortStation.h"
 
-int isValidBrackets(char originalString[]){
-    Stack* bracketsStack = stack_new();
+int isValidBrackets(char originalString[])
+{
+    Stack* bracketsStack = stackNew();
 
     for (int i = 0; i < strlen(originalString); i++) {
         char s = originalString[i];
-        
+
         if ((s == '(') || (s == '{') || (s == '[')) {
-            stack_push(bracketsStack, s);
-            
-        } else if ((s == ')' && bracketsStack == NULL) ||
-                   (s == '}' && bracketsStack == NULL) ||
-                   (s == ']' && bracketsStack == NULL)) {
-            //printf("String doesn't follow rules.\n");
+            stackPush(bracketsStack, s);
+
+        } else if ((s == ')' && bracketsStack == NULL)
+            || (s == '}' && bracketsStack == NULL)
+            || (s == ']' && bracketsStack == NULL)) {
             return 0;
 
-        } else if ((s == ')' && stack_peek(bracketsStack) == '(') ||
-                   (s == '}' && stack_peek(bracketsStack) == '{') ||
-                   (s == ']' && stack_peek(bracketsStack) == '[')) {
-            stack_pop(bracketsStack);
+        } else if ((s == ')' && stackPeek(bracketsStack) == '(')
+            || (s == '}' && stackPeek(bracketsStack) == '{')
+            || (s == ']' && stackPeek(bracketsStack) == '[')) {
+            stackPop(bracketsStack);
 
         } else {
-            //printf("String doesn't follow rules.\n");
             return 0;
         }
     }
     if (bracketsStack != NULL && bracketsStack->top != NULL) {
-        //printf("String doesn't follow rules.\n");
         return 0;
     }
-    //printf("Balance of brackets is keep.\n");
     return 1;
 }
 
@@ -41,6 +38,7 @@ int main(void)
     char originalString[100];
     scanf("%99s", originalString);
     int result = isValidBrackets(originalString);
+
     if (result == 0) {
         printf("String doesn't follow rules.\n");
         return -1;

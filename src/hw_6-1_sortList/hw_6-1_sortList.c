b9d7f2706a19263a4d9648e6ad28209e92ea4380
ada1ra <adada0901@gmail.com>
2025-11-17 23:54:14 +0300

    Delete main part, struct declaration and rename listNew

--- src/hw_6-1_sortList/hw_6-1_sortList.c
+++ src/hw_6-1_sortList/hw_6-1_sortList.c
@@ -2,20 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// структура узла списка
-typedef struct Node {
-    int data; // данные узла
-    struct Node* next; // указатель на следующий узел
-} Node;
-
-// структура списка
-typedef struct {
-    Node* head; // указатель на начало списка
-    int size; // размер списка
-} List;
-
 // создание нового пустого списка
-List* listNew()
+List* listCreate()
 {
     List* list = (List*)malloc(sizeof(List));
     if (list == NULL) {
@@ -168,75 +156,4 @@ int listContain(List* list, int value)
         position++;
     }
     return -1;
-}
-
-// основная функция программы
-int main()
-{
-    List* list = listNew();
-    int command;
-    int value;
-
-    printf("Программа для работы с сортированным списком\n");
-
-    do {
-        // вывод меню
-        printf("\nМеню операций:\n");
-        printf("0 - выйти\n");
-        printf("1 - добавить значение в сортированный список\n");
-        printf("2 - удалить значение из списка\n");
-        printf("3 - распечатать список\n");
-        printf("Выберите операцию: ");
-
-        scanf("%d", &command);
-
-        switch (command) {
-        case 0:
-            // выход из программы
-            printf("Завершение работы программы\n");
-            break;
-
-        case 1:
-            // добавление значения в сортированный список
-            printf("Введите значение для добавления: ");
-            scanf("%d", &value);
-
-            // находим позицию для сохранения сортировки
-            int position = listPosition(list, value);
-            listInsert(list, position, value);
-
-            printf("Значение %d добавлено в позицию %d\n", value, position);
-            break;
-
-        case 2:
-            // удаление значения из списка
-            printf("Введите значение для удаления: ");
-            scanf("%d", &value);
-
-            // проверяем наличие значения в списке
-            int removePosition = listContain(list, value);
-            if (removePosition != -1) {
-                listRemove(list, removePosition);
-                printf("Значение %d удалено из списка\n", value);
-            } else {
-                printf("Значение %d не найдено в списке\n", value);
-            }
-            break;
-
-        case 3:
-            // печать списка
-            listPrint(list);
-            break;
-
-        default:
-            printf("Неизвестная команда, попробуйте снова\n");
-            break;
-        }
-
-    } while (command != 0);
-
-    // освобождение памяти перед выходом
-    listDelete(list);
-
-    return 0;
 }
